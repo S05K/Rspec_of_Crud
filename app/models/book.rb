299d50869ed0dc12861d19090700ce403775c400
SO5K <sr966903@gmail.com>
2024-01-19 10:52:51 +0530

    spec for model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,12 @@
 class Book < ApplicationRecord
     self.table_name = :books
     validates :price, presence: true
+    validate :check_name
+
+    def check_name
+        if name == ""
+            errors.add(:name, "can't be an empty string")
+         end
+    end
     
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,21 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe Book, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe Book, type: :model do
+    let(:book) { FactoryBot.build(:book) }
+
+    context 'should validate' do 
+    it 'with name and price is present' do
+        expect(book).to be_valid
+    end
+
+    it 'when price is not present' do 
+        book.price = nil    
+        expect(book).not_to be_valid
+    end
+
+    it 'when name is an empty string' do 
+        book.name=""
+        expect(book).not_to be_valid
+    end
+ end
+end
